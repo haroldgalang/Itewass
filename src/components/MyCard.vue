c4202bfb24b3a9ed1713c70ffdb5cbbd4546dd85
haroldgalang <galangharold56@gmail.com>
2025-02-26 21:39:12 +0800

    my card update

--- src/components/MyCard.vue
+++ src/components/MyCard.vue
@@ -11,33 +11,15 @@
       <h4
         class="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased"
       >
-        Harold P. Galang
+        Galang, Harold P.
       </h4>
       <p
-        class="block bg-gradient-to-tr from-pink-600 to-pink-400 bg-clip-text font-sans text-base font-medium leading-relaxed text-transparent antialiased"
+        class="block bg-gradient-to-tr from-pink-600 to-pink-400 bg-clip-text font-sans text-base font-medium leading-relaxed text-transparent antialiased mb-5"
       >
         Student / Front-end Developer
-      </p>
+      </p class="">
+      <p>Brgy. Gulod, Cabuyao City, Laguna</p>
     </div>
-    <div class="flex justify-center gap-7 p-6 pt-2">
-      <a
-        href="#facebook"
-        class="block bg-gradient-to-tr from-blue-600 to-blue-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased"
-      >
-        <i class="fab fa-facebook" aria-hidden="true"></i>
-      </a>
-      <a
-        href="#twitter"
-        class="block bg-gradient-to-tr from-light-blue-600 to-light-blue-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased"
-      >
-        <i class="fab fa-twitter" aria-hidden="true"></i>
-      </a>
-      <a
-        href="#instagram"
-        class="block bg-gradient-to-tr from-purple-600 to-purple-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased"
-      >
-        <i class="fab fa-instagram" aria-hidden="true"></i>
-      </a>
-    </div>
+    
   </div>
 </template>
